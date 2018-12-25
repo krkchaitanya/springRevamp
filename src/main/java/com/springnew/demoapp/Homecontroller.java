441b402803820daaebb2c8500b7fb8717e7cbff4
krk chaitanya <krkchaitanya42@gmail.com>
2018-12-25 13:15:57 -0700

    spring MVC init and rendering sample jsp file from the controller

---
+++ src/main/java/com/springnew/demoapp/Homecontroller.java
@@ -0,0 +1,13 @@
+package com.springnew.demoapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Homecontroller {
+    @RequestMapping("/")
+    public String showPage() {
+        return "main-menu";
+    }
+}
+
