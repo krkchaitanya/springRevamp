d380d1b0d59e257f565bb490c7b1fc0bcbda468a
krk chaitanya <krkchaitanya42@gmail.com>
2019-05-27 23:08:59 -0600

    implementing findany and orelse in streams

---
+++ src/main/java/com/samplePojos/Person.java
@@ -0,0 +1,35 @@
+package com.samplePojos;
+
+public class Person {
+    private String name;
+    private Number age;
+
+    public Person(String name, Number age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Number getAge() {
+        return age;
+    }
+
+    public void setAge(Number age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
--- src/main/java/com/streamsRevamp/NowJava8.java
+++ src/main/java/com/streamsRevamp/NowJava8.java
@@ -1,5 +1,7 @@
 package com.streamsRevamp;
 
+import com.samplePojos.Person;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -21,9 +23,28 @@ public class NowJava8 {
                 .filter(fw -> !("jquery".equals(fw)))
                 .collect(Collectors.toList());
         streamLst.forEach(ele -> System.out.println(ele));
+
+
+        List<Person> persons = Arrays.asList(
+                new Person("mikeljohn", 23),
+                new Person("testperson", 34),
+                new Person("johnson", 42),
+                new Person("Kristen steweret", 25)
+        );
+        System.out.println(" --Person OBJ -- " + getStudentByName(persons, "johnson"));
+
+
+        Person resultPerson8 = persons.stream()
+                .filter(per -> "johnson".equalsIgnoreCase(per.getName()))
+                .findAny()
+                .orElse(null);
+        System.out.println(" -- resultPerson8 -- " + resultPerson8);
+
     }
 
 
+
+//    METHODS
     private static List<String> getFilterdFrameworklist(List<String> fwList, String filter) {
         List<String> resultLst = new ArrayList<>();
         for(String fw : fwList) {
@@ -32,6 +53,16 @@ public class NowJava8 {
             }
         }
         return resultLst;
+    }
+
+    private static Person getStudentByName(List<Person> persons, String personFilter) {
+        Person resultPErson = null;
+        for (Person person : persons) {
+            if (personFilter.equalsIgnoreCase(person.getName())) {
+                resultPErson = person;
+            }
+        }
+        return resultPErson;
     }
 
 
